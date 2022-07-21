a6e8caaa4b53c0b5cea083273729a281bad56675
gary <gary.white@lodgit.com>
2022-07-21 13:33:09 +0200

    code working

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // String.prototype.toJadenCase = function () {
-//     //...
-// };
+//     let splitStr = this.toLowerCase().split(' ');
+//       for (let i = 0; i < splitStr.length; i++) {
+//           splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+//       }
+//       return splitStr.join(" ");
+//   };
 
 function jadenCase(s) {
-    return s;
+    var splitStr = s.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(" ");
 }
 
 console.log(jadenCase("How can mirrors be real if our eyes aren't real")); // "How Can Mirrors Be Real If Our Eyes Aren't Real"
