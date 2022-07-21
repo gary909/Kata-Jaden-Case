879e56d196723f60a4d97b88809a19be6bf1e65f
gary <gary.white@lodgit.com>
2022-07-21 13:24:50 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+// String.prototype.toJadenCase = function () {
+//     //...
+// };
+
+function jadenCase(s) {
+    return s;
+}
+
+console.log(jadenCase("How can mirrors be real if our eyes aren't real")); // "How Can Mirrors Be Real If Our Eyes Aren't Real"
